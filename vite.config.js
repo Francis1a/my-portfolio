dacbe6ec4795364608155d668f658933bee2d2e8
Francis1a <52089780+Francis1a@users.noreply.github.com>
2024-06-07 17:01:23 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),svgr()],
+  resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+         extensions: [
+          '.jsx',
+         ]
+      },
   base: './',
 })
